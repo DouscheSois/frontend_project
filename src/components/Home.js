70375ef6cd7bac6f43cfc0e9d26caa82d8b5e47d
Roger <roger.alex.aguilar@gmail.com>
2020-11-20 15:26:18 -0800

    restyled

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,15 +2,30 @@ import React from "react";
 
 import Matt from "../images/matt.jpg";
 
-import { Row, Col, Image } from "react-bootstrap";
+import { Row, Col, Image, Button, Container, Jumbotron } from "react-bootstrap";
 
 const Home = () => {
   return (
-    <Row>
-      <Col>
-        <Image width="auto" src={Matt} fluid />
-      </Col>
-    </Row>
+    <div className="top-container">
+      <div className="background-filter">
+        <Jumbotron className="transparent my-2 py-3" fluid>
+          <h1>Full Name</h1>
+          <p>Matthew White Ridley the 5th Viscount Ridley</p>
+        </Jumbotron>
+        <Jumbotron className="transparent my-2 py-3" fluid>
+          <h1>Full Name</h1>
+          <p>Matthew White Ridley the 5th Viscount Ridley</p>
+        </Jumbotron>
+        <Jumbotron className="transparent my-2 py-3" fluid>
+          <h1>Full Name</h1>
+          <p>Matthew White Ridley the 5th Viscount Ridley</p>
+        </Jumbotron>
+        <Jumbotron className="transparent my-2 py-3" fluid>
+          <h1>Full Name</h1>
+          <p>Matthew White Ridley the 5th Viscount Ridley</p>
+        </Jumbotron>
+      </div>
+    </div>
   );
 };
 
